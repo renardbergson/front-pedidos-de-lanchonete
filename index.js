5f77838ceec6d454f4f6b93915c2be9803b1900d
Renard Bergson <renardrock@gmail.com>
2023-08-11 18:41:01 -0300

    criando página de login, criando página de listagem de produtos, corrigindo importação de arquivos externos no CSS

---
+++ index.js
@@ -0,0 +1,28 @@
+const _loginForm = document.querySelector('#loginForm')
+const _userSelector = document.querySelector('#userSelector')
+
+_userSelector.onchange = () => {
+    const _adminField = document.querySelector('#adminField')
+    const _userField = document.querySelector('#userField')
+    const _loginInputFields = document.querySelectorAll('.loginInputField')
+
+    _loginInputFields.forEach(item => {item.value = ''})
+
+    if (_userSelector.value === 'user') {
+        _adminField.style.display = 'none'
+        _userField.style.display = 'block'
+    } 
+
+    if (_userSelector.value === 'admin') {
+        _userField.style.display = 'none'
+        _adminField.style.display = 'block'
+    }
+}
+
+_loginForm.onsubmit = e => {
+    e.preventDefault()
+
+    if (_loginForm.userID.value === 'admin' && _loginForm.userPass.value === 'admin') {
+        window.location.href = 'admin-list.html';
+    }
+}
