a7706e500319d0fe7de078450f56598feaea4025
Renard Bergson <renardrock@gmail.com>
2023-09-13 13:08:22 -0300

    modificando js para INDEX e LOGGED USER

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const productsURL = 'http://localhost:8080/products'
 const customersURL = 'http://localhost:8080/customers'
 
 // BODY
+const _index = document.querySelector('#index')
 const _login = document.querySelector('#login')
 const _loggedUser = document.querySelector('#loggedUser')
 const _listProductsAdmin = document.querySelector('#listProductsAdmin')
@@ -46,34 +47,36 @@ if (_login) {
 
 // ============================== PRODUCTS ==============================
 // GET - Index
-document.body.onload = () => {    
+if (_index) {
     const _products = document.querySelector('#products')
     let productsHTML = ''
 
-    if (_products != null) {
-        fetch(productsURL)
-        .then(response => response.json())
-        .then(data => data.forEach(product => {
-            productsHTML += `
-                <div class="product">
-                    <img src="https://boracolorir.com.br/wp-content/uploads/2022/02/desenhos-de-comida-para-colorir-3.jpg" alt="foto de lanche" class="productPreview">
-    
-                    <div class="productDetails">
-                        <h4 class="productTitle">${product.name}</h4>
-    
-                        <p class="productDescription">${product.description}</p>
-    
-                        <p>Valor <span class="productPrice">${product.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</span> </p>
-                    </div>
-    
-                    <button class="getItemBtn">Peça Já!</button>
-                </div>
-            `
-            _products.innerHTML = productsHTML
+    fetch(productsURL)
+    .then(response => response.json())
+    .then(data => data.forEach(product => {
+        productsHTML += `
+            <div class="product">
+                <img src="https://boracolorir.com.br/wp-content/uploads/2022/02/desenhos-de-comida-para-colorir-3.jpg" alt="foto de lanche" class="productPreview">
 
-            getItem()
-        }))
-    }
+                <div class="productDetails">
+                    <h4 class="productTitle">${product.name}</h4>
+
+                    <p class="productDescription">${product.description}</p>
+
+                    <p>Valor <span class="productPrice">${product.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</span> </p>
+                </div>
+
+                <button class="getItemBtn">Peça Já!</button>
+            </div>
+        `
+        _products.innerHTML = productsHTML
+
+        const _getItemBtns = document.querySelectorAll('.getItemBtn')
+
+        _getItemBtns.forEach(btn => {
+            btn.onclick = () => {alert('Quase lá! Faça login para continuar!')}
+        })
+    }))
 }
 
 // GET - (admin)
@@ -168,17 +171,39 @@ if (_addNewProductAdmin) {
     }
 }
 
-// purchase  
-function getItem () {
-    const _getItemBtns = document.querySelectorAll('.getItemBtn')
+// POST - (user)
+if (_loggedUser) {
+    const _products = document.querySelector('#products')
+    let productsHTML = ''
 
-    _getItemBtns.forEach(button => {
-        button.onclick = () => {
-            const _main = document.querySelector('#main')
-            const _posOrderSplash = document.querySelector('#posOrderSplash')
-            const _gotItBtn = document.querySelector('#gotItBtn')
+    fetch(productsURL)
+    .then(response => response.json())
+    .then(data => data.forEach(product => {
+        productsHTML += `
+            <div class="product">
+                <img src="https://boracolorir.com.br/wp-content/uploads/2022/02/desenhos-de-comida-para-colorir-3.jpg" alt="foto de lanche" class="productPreview">
 
-            if (_main) {
+                <div class="productDetails">
+                    <h4 class="productTitle">${product.name}</h4>
+
+                    <p class="productDescription">${product.description}</p>
+
+                    <p>Valor <span class="productPrice">${product.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</span> </p>
+                </div>
+
+                <button class="getItemBtn" data-id=${product._id}>Peça Já!</button>
+            </div>
+        `
+        _products.innerHTML = productsHTML
+
+        const _getItemBtns = document.querySelectorAll('.getItemBtn')
+
+        _getItemBtns.forEach(button => {
+            button.onclick = function () {
+                const _main = document.querySelector('#main')
+                const _posOrderSplash = document.querySelector('#posOrderSplash')
+                const _gotItBtn = document.querySelector('#gotItBtn')
+    
                 _main.classList.add('almostHidden')
                 _posOrderSplash.classList.add('visible')
         
@@ -186,9 +211,9 @@ function getItem () {
                     _posOrderSplash.classList.remove('visible')
                     _main.classList.remove('almostHidden')
                 }
-            }
-        }  
-    })
+            }  
+        })
+    }))
 }
 
 // cancel order
